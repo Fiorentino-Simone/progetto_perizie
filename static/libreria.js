48cebb41f861d8c4fce15a82ad1318948c425dbf
Simone Fiorentino <simone.fiorentino@cambieri.it>
2022-12-29 20:53:52 +0100

    edit static files

--- static/libreria.js
+++ static/libreria.js
@@ -1,35 +1,44 @@
-"use strict";
-
-function inviaRichiesta(method, url, parameters={}) {
+function inviaRichiesta(method, url, parameters = {}) {
 	let contentType;
-	if(method.toUpperCase()=="GET")
-		contentType="application/x-www-form-urlencoded;charset=utf-8";
-	else{
-		contentType = "application/json;charset=utf-8"
-        parameters = JSON.stringify(parameters);
+	if (method.toUpperCase() == "GET") {
+		contentType = "application/x-www-form-urlencoded; charset=UTF-8"
 	}
-    return $.ajax({
-        "url": url,
-		"data": parameters,
-		"type": method,   
-		"contentType": contentType, 
-        "dataType": "json",   // default      
-        "timeout": 5000,      // default 
-		// L'intestazione content-type NON viene creata di default
-		"headers":{"Content-Type":contentType}
-    });	
+	else {
+		contentType = "application/json; charset=utf-8"
+		parameters = JSON.stringify(parameters);
+	}
+
+	return $.ajax({
+		url: url, //default: currentPage
+		type: method,
+		data: parameters,
+		contentType: contentType,
+		dataType: "json",
+		timeout: 5000,
+		beforeSend: function (jqXHR) {
+			if ("token" in localStorage) {
+				let token = localStorage.getItem("token");
+				console.log("SEND -- ", token)
+				jqXHR.setRequestHeader("Authorization", token);
+			}
+		},
+		success: function (data, textStatus, jqXHR) {
+			let token = jqXHR.getResponseHeader('Authorization')
+			console.log("RECEIVE -- ", token)
+			localStorage.setItem("token", token)
+		}
+	});
 }
 
 
-function errore(jqXHR, text_status, string_error) {
-    if (jqXHR.status == 0)
-        alert("Connection Refused or Server timeout");
+
+function errore(jqXHR, testStatus, strError) {
+	if (jqXHR.status == 0)
+		alert("Connection refused or Server timeout");
 	else if (jqXHR.status == 200)
-        alert("Formato dei dati non corretto : " + jqXHR.responseText);
-    else
-        alert("Server Error: " + jqXHR.status + " - " + jqXHR.responseText);
-}
-
-function generaNumero(a, b){
-	return Math.floor((b - a + 1) * Math.random()) + a;
+		alert("Formato dei dati non corretto : " + jqXHR.responseText);
+	else if (jqXHR.status == 403) //accesso negato (token scaduto)
+		window.location.href = "login.html"
+	else
+		alert("Server Error: " + jqXHR.status + " - " + jqXHR.responseText);
 }
