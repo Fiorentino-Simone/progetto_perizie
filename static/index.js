45f964c1a2ed645ffc2493b600f6e503c09f377c
Simone Fiorentino <simone.fiorentino@cambieri.it>
2022-12-29 22:35:43 +0100

    adding arrow for section in mobile and set media query

--- static/index.js
+++ static/index.js
@@ -1,6 +1,24 @@
 "use strict";
 
 $(document).ready(function () {
-  console.log("Heigth: " + $(window).height());
-  console.log("Width: " + $(window).width());
+  let boolClicked=false;
+
+  //se su mobile allora uso la funzione lampeggia
+  if ($(window).width() < 768)
+    lampeggia();
+
+
+  /************************FUNCTIONS AND REQUEST *****************************/
+  function lampeggia() { //funzione per il lampeggio della freccia che porta alla scelta delle option
+    let opacity = {
+      "opacity": "1"
+    };
+    $("#arrow").animate(opacity, 1600, function () { //utilizzo la tecnica della ricorsione, che va all'infinito
+      $(this).animate({ "opacity": "0" }, 500, function () {
+        if (!boolClicked) {
+          lampeggia();
+        }
+      });
+    });
+  }
 });
